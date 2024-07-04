5c30196d68f47aba93d408f60f0c5dde6946e67b
Nicholas Ng <nicholasnkk@gmail.com>
2024-07-04 17:28:44 +0800

    added recolor functions
    for QIcon, QPixmap, QImage

---
+++ src/media/media.cpp
@@ -0,0 +1,24 @@
+#include "media/media.hpp"
+
+QIcon Media::recolor(const QIcon &icon, const QColor &color, const QSize &size)
+{
+    return QIcon(recolor(icon.pixmap(size), color));
+}
+
+QPixmap Media::recolor(const QPixmap &pixmap, const QColor &color)
+{
+    return QPixmap::fromImage(recolor(pixmap.toImage(), color));
+}
+
+QImage Media::recolor(const QImage &image, const QColor &color)
+{
+    QImage result(image.size(), image.format());
+    auto mutableColor = color; // necessary to reassign alpha value
+    for (int y = 0; y < image.height(); y++)
+        for (int x = 0; x < image.width(); x++)
+        {
+            mutableColor.setAlpha(image.pixelColor(x, y).alpha());
+            result.setPixelColor(x, y, mutableColor);
+        }
+    return result;
+}
